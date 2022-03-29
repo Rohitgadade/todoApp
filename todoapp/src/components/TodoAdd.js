8ab6b47026dac698dcfd9e18053b829676de27cc
Rohitgadade <rohitgadade1997@gmail.com>
2022-03-29 12:55:18 +0530

    done

--- todoapp/src/components/TodoAdd.js
+++ todoapp/src/components/TodoAdd.js
@@ -1,4 +1,4 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { add_Todos } from "../store/actions";
 import TodoList from "./TodoList";
